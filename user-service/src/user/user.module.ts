b644ec922b23eee0c0ca503dd2af39983edcbb92
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-14 22:55:10 +0530

    Fix: Resolve error in user profile display.

--- user-service/src/user/user.module.ts
+++ user-service/src/user/user.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.model';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './model/user.schema';
+import { AuthModule } from '../auth/auth.module';
+import { Role, RoleSchema } from './model/role.schema';
+import { RoleService } from './service/role.service';
+import { RoleController } from './controller/role.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Role.name, schema: RoleSchema }
+    ]),
+    AuthModule
   ],
-  controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, RoleService],
+  controllers: [UserController, RoleController]
 })
 export class UserModule {}
