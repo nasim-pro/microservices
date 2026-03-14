3f711bbf10d1a61d259dbb25a73b75f86337b899
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-14 22:54:40 +0530

    fix

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,19 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.model';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env', }),
-    UserModule,
-    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }), // Loads .env file
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+    UserModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements OnModuleInit {
+  constructor(@InjectConnection() private connection: Connection) { }
+  private readonly logger = new Logger('Database');
+  
+  onModuleInit() {
+    if (this.connection.readyState === 1) {
+      this.logger.log('MongoDB connected successfully');
+    } else {
+      this.logger.error('MongoDB connection failed');
+    }
+  }
+}
