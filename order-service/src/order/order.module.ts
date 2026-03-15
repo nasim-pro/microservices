59751b51edb356fef4998f373497804b758eb1be
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 05:33:40 +0530

    refactor(order.module): refactor to use NestJS Axios and clients for better dependency injection

--- order-service/src/order/order.module.ts
+++ order-service/src/order/order.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { HttpModule } from '@nestjs/axios';
+import { UserClient } from '../clients/user.client';
+import { ProductClient } from '../clients/product.client';
 
 @Module({
-  providers: [OrderService],
+  imports: [HttpModule],
+
+  providers: [OrderService, UserClient, ProductClient],
   controllers: [OrderController]
 })
 export class OrderModule {}
