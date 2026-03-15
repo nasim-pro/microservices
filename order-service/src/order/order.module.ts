fb4b2ba4bbc2f558c95674c1f4a85b544ddb9be3
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 18:09:14 +0530

    feat(order): add order model to order module using Mongoose

--- order-service/src/order/order.module.ts
+++ order-service/src/order/order.module.ts
@@ -4,9 +4,17 @@ import { OrderController } from './order.controller';
 import { HttpModule } from '@nestjs/axios';
 import { UserClient } from '../clients/user.client';
 import { ProductClient } from '../clients/product.client';
+import { Order, OrderSchema } from './order.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+    ]),
+    
+    HttpModule
+  ],
 
   providers: [OrderService, UserClient, ProductClient],
   controllers: [OrderController]
