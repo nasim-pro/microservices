a6ba99a0abbd1d1070bd722d47c44b7e79b3e082
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 05:33:34 +0530

    feat(order-service): add CreateOrderDto to OrderController

--- order-service/src/order/order.controller.ts
+++ order-service/src/order/order.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { CreateOrderDto } from './order.dto';
 
 @Controller('orders')
 export class OrderController {
-    constructor(private readonly orderService: OrderService) { }
+
+    constructor(private orderService: OrderService) { }
 
     @Post()
-    createOrder(@Body() body: any) {
-        return this.orderService.createOrder(body);
+    create(@Body() dto: CreateOrderDto) {
+        return this.orderService.createOrder(dto);
     }
 }
