32295ac069e52d7634991c86b7383e9fd613acc6
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 18:09:34 +0530

    fix(server): update server name from 'api' to 'product-service'

--- product-service/server.ts
+++ product-service/server.ts
@@ -10,7 +10,7 @@ const port = parseInt(process.env.PORT || '3000', 10);
 app.use(express.json());
 
 // Routes
-app.use('/api', routes); // Use the indexed routes
+app.use('/', routes); // Use the indexed routes
 
 connectDatabase(); // Wait for the database connection
-app.listen(port, () => { console.log(`Server is running on port ${port}`) });
+app.listen(port, () => { console.log(`product-service is running on port ${port}`) });
