7911dc9b4108d4f8e93eff3cbcd9901a192b73f8
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 18:08:39 +0530

    feat(order-service): add Mongoose module for database connection

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Loads .env file
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
     OrderModule,
   ],
   controllers: [AppController],
