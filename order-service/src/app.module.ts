bc9c291ff674bfae7ad6ac07754496e43f626e37
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 05:33:22 +0530

    feat(app.module): add configuration for environment variables using ConfigModule

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrderModule } from './order/order.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: 'USER_SERVICE',
-        transport: Transport.REDIS,
-        options: { host: 'localhost', port: 6379 },
-      },
-      {
-        name: 'PRODUCT_SERVICE',
-        transport: Transport.REDIS,
-        options: { host: 'localhost', port: 6379 },
-      },
-    ]),
+    ConfigModule.forRoot({ isGlobal: true }), // Loads .env file
     OrderModule,
   ],
   controllers: [AppController],
