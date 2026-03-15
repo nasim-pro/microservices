7149c7b86d3b11fb0f94050fb2958d9217003098
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 05:33:29 +0530

    refactor(src/main.ts): use ValidationPipe in Nest.js application.

--- order-service/src/main.ts
+++ order-service/src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.REDIS,
-    options: {
-      host: 'localhost',
-      port: 6379,
-    },
-  });
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,            // remove unknown fields
+      forbidNonWhitelisted: true, // throw error if extra fields
+      transform: true,            // auto transform DTO types
+    }),
+  );
   await app.startAllMicroservices();
-  await app.listen(3002);
+  const port = process.env.PORT ? Number(process.env.PORT) : 2026;
+  await app.listen(port);
 }
 bootstrap();
