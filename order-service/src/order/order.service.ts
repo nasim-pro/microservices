67b0152afe80ed5516485872afeb9cf2751a6bbf
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-15 18:09:24 +0530

    feat(order-service): add authentication to order creation and remove redundant fields from order model

--- order-service/src/order/order.service.ts
+++ order-service/src/order/order.service.ts
@@ -2,35 +2,36 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { UserClient } from '../clients/user.client';
 import { ProductClient } from '../clients/product.client';
 import { CreateOrderDto } from './order.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order, OrderDocument } from './order.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class OrderService {
 
     constructor(
         private userClient: UserClient,
-        private productClient: ProductClient
+        private productClient: ProductClient,
+        @InjectModel(Order.name)
+        private readonly orderModel: Model<OrderDocument>
     ) { }
 
-    async createOrder(dto: CreateOrderDto) {
+    async createOrder(dto: CreateOrderDto, authHeader?: string) {
 
-        const user = await this.userClient.getUser(dto.userId);
+        const user = await this.userClient.getUser(dto.userId, authHeader);
         if (!user) {
             throw new BadRequestException('User not found');
         }
 
-        const product = await this.productClient.getProduct(dto.productId);
+        const product = await this.productClient.getProduct(dto.productId, authHeader);
+
         if (!product) {
             throw new BadRequestException('Product not found');
         }
 
-        const order = {
-            id: Date.now().toString(),
-            userId: dto.userId,
-            productId: dto.productId,
-            quantity: dto.quantity,
-            totalPrice: product.price * dto.quantity
-        };
+        const order = this.orderModel.create(dto)
 
         return order;
     }
 }
+
