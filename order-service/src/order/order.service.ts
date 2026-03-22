162564b110451be42b76c9c0fbe9a51a1e5625b5
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-22 20:59:15 +0530

    fix(order-service): Add validation for product stock when creating an order. Added a check to ensure that the product has enough in stock before creating an order and published an event asynchronously after creating the order.

--- order-service/src/order/order.service.ts
+++ order-service/src/order/order.service.ts
@@ -5,6 +5,7 @@ import { CreateOrderDto } from './order.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order, OrderDocument } from './order.schema';
 import { Model } from 'mongoose';
+import { sendOrderCreatedEvent } from '../kafka/producer';
 
 @Injectable()
 export class OrderService {
@@ -16,28 +17,35 @@ export class OrderService {
         private readonly orderModel: Model<OrderDocument>
     ) { }
 
-    async createOrder(dto: CreateOrderDto, authHeader?: string) {
-            const user = await this.userClient.getUser(dto.userId, authHeader);
-            if (!user) {
-                throw new BadRequestException('User not found');
-            }
 
-            const product = await this.productClient.getProduct(dto.productId, authHeader);
-
-            if (!product) {
-                throw new BadRequestException('Product not found');
-            }
-
-            const left = product.stock - dto.quantity;
-
-            if (left < 0) {
-                throw new BadRequestException(`Product out of stock only ${product.stock} stocks are left`)
-            }
-
-            const updated = this.productClient.updateProduct(product._id, { stock: left }, authHeader)
-            const order = this.orderModel.create(dto)
-
-            return order;
+async createOrder(dto: CreateOrderDto, authHeader ?: string) {
+    const user = await this.userClient.getUser(dto.userId, authHeader);
+    if (!user) {
+        throw new BadRequestException('User not found');
     }
+
+    const product = await this.productClient.getProduct(dto.productId, authHeader);
+    if (!product) {
+        throw new BadRequestException('Product not found');
+    }
+
+    if (product.stock < dto.quantity) {
+        throw new BadRequestException(
+            `Product out of stock. Only ${product.stock} left`
+        );
+    }
+
+    // Create order 
+    const order = await this.orderModel.create(dto);
+
+    //Publish event asynchronously
+    await sendOrderCreatedEvent({
+        orderId: order._id.toString(),
+        productId: dto.productId,
+        quantity: dto.quantity,
+    });
+
+    return order;
+}
 }
 
