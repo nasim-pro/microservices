3d3ceb0ea0b2933a51955f8a6b3d6da6af5a49a4
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-22 20:59:00 +0530

    feat(order-service): add Kafka producer connection in main.ts

--- order-service/src/main.ts
+++ order-service/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ValidateTokenInterceptor } from './validate/validate-jwt-token';
 import { JwtService } from '@nestjs/jwt';
+import { connectProducer } from './kafka/producer';
 
 async function bootstrap() {
 
@@ -16,9 +17,12 @@ async function bootstrap() {
   );
   app.useGlobalInterceptors(new ValidateTokenInterceptor())
   const port = process.env.PORT ? Number(process.env.PORT) : 2026;
+  
   await app.listen(port);
 
   const logger = new Logger('Bootstrap');
   logger.log(`order-service is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  await connectProducer(); // ✅ connect Kafka
+
 }
 bootstrap();
