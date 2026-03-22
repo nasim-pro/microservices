98baeacf53b2b6ac1b5abc32a90134a0109bea93
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-22 11:02:30 +0530

    fix(user.client.ts): handle error in getUser method

--- order-service/src/clients/user.client.ts
+++ order-service/src/clients/user.client.ts
@@ -8,10 +8,15 @@ export class UserClient {
     constructor(private httpService: HttpService, ) { }
 
     async getUser(userId: string, authHeader?: string) {
+        try {
         const config = authHeader ? { headers: { Authorization: authHeader } } : {};
         const response = await firstValueFrom(
             this.httpService.get(`http://user-service:2024/users/${userId}`, config)
         );
         return response.data;
+        } catch (err: any) {
+            console.error(err.message)
+            return null
+        }
     }
 }
